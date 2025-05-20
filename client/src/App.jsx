ed07d88f43ba30826f9534feadcd530efd32dfda
Arash <arash8877@gmail.com>
2025-05-20 12:50:12 +0200

    create hero part  in home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import { useLocation } from "react-router-dom"
+import {Routes, Route, useLocation } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import Home from "./pages/Home";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
     </div>
   )
 }
