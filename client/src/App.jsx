a68995e58e4617e7c92aaac96458699a1d34062f
Arash <arash8877@gmail.com>
2025-05-21 11:36:12 +0200

    footer done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route, useLocation } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,6 +13,7 @@ const App = () => {
           <Route path="/" element={<Home/>} />
         </Routes>
       </div>
+      <Footer />
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import FeatureDestination from "../components/FeatureDestination";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import NewsLetter from "../components/NewsLetter";
 import Testimonial from "../components/Testimonial";
