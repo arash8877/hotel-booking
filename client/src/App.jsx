7bdb6b18e8ca23bf535f7646eb1545798e0c2abe
Arash <arash8877@gmail.com>
2025-05-21 14:57:33 +0200

    create all-rooms route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route, useLocation } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -11,6 +12,7 @@ const App = () => {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/rooms" element={<AllRooms/>} />
         </Routes>
       </div>
       <Footer />
