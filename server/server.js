b62690dd6812d47ba629dbd202a40bc933b7cca6
Arash <arash8877@gmail.com>
2025-05-22 21:59:21 +0200

    modify server.js

--- server/server.js
+++ server/server.js
@@ -6,3 +6,7 @@ const app = express();
 app.use(cors()); //Enable cross-origin resource sharing
 
 app.get("/", (req, res) => res.send("API is working!"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
