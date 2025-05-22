e85870e0d50aa590c7a9c4a4b1a9e4362c010038
Arash <arash8877@gmail.com>
2025-05-22 09:55:57 +0200

    my-booking page ui done

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -34,7 +34,7 @@ const MyBookings = () => {
               <div className="flex flex-col gap-1.5 max-md:mt-3 min-md:ml-4">
                 <p className="font-playfair text-2xl">
                   {item.hotel.name}
-                  <span className="font-inter text-sm">({item.room.roomType})</span>
+                  <span className="font-inter text-sm"> ({item.room.roomType})</span>
                 </p>
 
                 <div className="flex items-center gap-1 text-sm text-gray-500">
@@ -51,10 +51,30 @@ const MyBookings = () => {
             </div>
 
             {/* ------ Hotel Details ------ */}
-            <div></div>
+            <div className="flex flex-row md:items-center md:gap-12 mt-3 gap-8">
+                <div>
+                    <p>Check-In:</p>
+                    <p className="text-gray-500 text-sm">
+                        {new Date(item.checkInDate).toDateString}
+                    </p>
+                </div>
+                <div>
+                    <p>Check-Out:</p>
+                    <p className="text-gray-500 text-sm">
+                        {new Date(item.checkOutDate).toDateString}
+                    </p>
+                </div>
+
+            </div>
 
             {/* ------ Hotel Details ------ */}
-            <div></div>
+            <div className="flex flex-col items-start justify-center pt-3">
+                <div className="flex items-center gap-2">
+                    <div className={`h-3 w-3 rounded-full ${item.isPaid ? 'bg-green-500' : 'bg-red-500'}`}></div>
+                    <p className={`text-sm ${item.isPaid ? 'text-green-500' : 'text-red-500'}`}>{item.isPaid ? 'Paid' : 'Unpaid'}</p>
+                </div>
+                {!item.isPaid && (<button className="px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer">Pay Now</button>)}
+            </div>
           </div>
         ))}
       </div>
