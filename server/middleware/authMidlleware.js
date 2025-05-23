6cbf86a25f0cf4760a3f7af227e14897fd6d69b6
Arash <arash8877@gmail.com>
2025-05-23 13:11:27 +0200

    create auth middleware

---
+++ server/middleware/authMidlleware.js
@@ -0,0 +1,14 @@
+import User from "../models/User.js";
+
+// Middleware to check if user is authenticated
+export const protect = async (req, res, next) => {
+  const { userId } = req.auth;
+
+  if (!userId) {
+    res.json({ success: false, message: "User not authenticated" }, 401);
+  } else {
+    const user = await User.findById(userId);
+    req.user = user;
+    next();
+  }
+};
