334261c3896d64936f8ee6132cb580bcace0adc6
Arash <arash8877@gmail.com>
2025-05-23 21:33:19 +0200

    create booking model

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,9 +1,17 @@
-import express from 'express';
-import { createRoom } from '../controllers/roomController';
-import { protect } from '../middleware/authMiddleware.js';
+import express from "express";
+import {
+  createRoom,
+  getOwnerRooms,
+  getRooms,
+  toggleRoomAvailability,
+} from "../controllers/roomController";
+import { protect } from "../middleware/authMiddleware.js";
 
 const roomRouter = express.Router();
 
-roomRouter.post('/', upload.array('images', 4), protect, createRoom)
+roomRouter.post("/", upload.array("images", 4), protect, createRoom);
+roomRouter.get("/", getRooms);
+roomRouter.get("/owner", protect, getOwnerRooms);
+roomRouter.post("/toggle-availability", protect, toggleRoomAvailability);
 
 export default roomRouter;
