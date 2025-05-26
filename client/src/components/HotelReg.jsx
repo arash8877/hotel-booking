bfcf6cc6a40236d2d3154f32525802321b8bbb74
Arash <arash8877@gmail.com>
2025-05-26 09:17:28 +0200

    manage dashboard button

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,6 +1,8 @@
 import { assets, cities } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 const HotelReg = () => {
+  const { setShowHotelReg } = useAppContext();
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70">
       <form className="flex bg-white rounded-xl max-w-4xl max-md:mx-2">
@@ -10,6 +12,7 @@ const HotelReg = () => {
             src={assets.closeIcon}
             alt="close-icon"
             className="absolute top-4 right-4 h-4 w-4 cursor-pointer"
+            onClick={() => setShowHotelReg(false)}
           />
           <p className="text-2xl font-semibold mt-6">Register Your Hotel</p>
           {/* --- Hotel Name --- */}
@@ -54,21 +57,21 @@ const HotelReg = () => {
           </div>
           {/* --- City Dropdown --- */}
           <div className="w-full mt-4 max-w-60 mr-auto">
-          <label htmlFor="city" className="font-medium text-gray-500">
-            City
-          </label>
-          <select
-            id="city"
-            className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
-            required
-          >
-            <option value="">Select City</option>
-            {cities.map((city) => (
-              <option key={city} value={city}>
-                {city}
-              </option>
-            ))}
-          </select>
+            <label htmlFor="city" className="font-medium text-gray-500">
+              City
+            </label>
+            <select
+              id="city"
+              className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
+              required
+            >
+              <option value="">Select City</option>
+              {cities.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
           </div>
           <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white mr-auto px-6 py-2 rounded cursor-pointer mt-6">
             Register
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { assets } from "../assets/assets.js";
-import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
+import { useClerk, UserButton } from "@clerk/clerk-react";
+import { useAppContext } from "../context/AppContext.jsx";
 
 const BookIcon = () => (
   <svg
@@ -35,9 +36,8 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const { openSignIn } = useClerk();
-  const { user } = useUser();
-  const navigate = useNavigate();
   const location = useLocation();
+  const { user, navigate, isOwner, setShowHotelReg } = useAppContext();
 
   useEffect(() => {
     if (location.pathname !== "/") {
@@ -84,14 +84,16 @@ const Navbar = () => {
             />
           </a>
         ))}
-        <button
-          className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
-            isScrolled ? "text-black" : "text-white"
-          } transition-all`}
-          onClick={() => navigate("/owner")}
-        >
-          Dashboard
-        </button>
+        {user && (
+          <button
+            className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
+              isScrolled ? "text-black" : "text-white"
+            } transition-all`}
+            onClick={() => (isOwner ? navigate("/owner") : setShowHotelReg(true))}
+          >
+            {isOwner ? "Dashboard" : "List Yor Hotel"}
+          </button>
+        )}
       </div>
 
       {/* Desktop Right */}
@@ -167,9 +169,9 @@ const Navbar = () => {
         {user && (
           <button
             className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all"
-            onClick={() => navigate("/owner")}
+            onClick={() => (isOwner ? navigate("/owner") : setShowHotelReg(true))}
           >
-            Dashboard
+            {isOwner ? "Dashboard" : "List Yor Hotel"}
           </button>
         )}
 
