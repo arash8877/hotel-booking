64549e7c94436520bce01620cbde39734cff386b
Arash <arash8877@gmail.com>
2025-05-27 10:21:27 +0200

    protect the dashboard route

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -1,21 +1,30 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../../components/hotelOwiner/Navbar"
-import Sidebar from "../../components/hotelOwiner/Sidebar"
-
-
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/hotelOwner/Navbar";
+import Sidebar from "../../components/hotelOwner/Sidebar";
+import { useAppContext } from "../../context/AppContext";
 
 const Layout = () => {
+  const { isOwner, navigate } = useAppContext();
+
+  // protect the dashboard route
+  useEffect(() => {
+    if (!isOwner) {
+      navigate("/");
+    }
+  }, [isOwner]);
+
   return (
     <div className="flex flex-col h-screen">
-        <Navbar />
-        <div className="flex h-full">
-            <Sidebar/>
-            <div className="flex-1 p-4 pt-10 md:px-10 h-full">
-                <Outlet/>
-            </div>
+      <Navbar />
+      <div className="flex h-full">
+        <Sidebar />
+        <div className="flex-1 p-4 pt-10 md:px-10 h-full">
+          <Outlet />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
